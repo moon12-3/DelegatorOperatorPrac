e3612890e4ad603c98ced20384c7df9319216543
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-08-31 14:48:18 +0900

    초기 커밋.

--- DelegatorOperatorPrac/Program.cs
+++ DelegatorOperatorPrac/Program.cs
@@ -8,8 +8,27 @@ namespace DelegatorOperatorPrac
 {
     internal class Program
     {
+        public delegate void SendString(string message);
+
         static void Main(string[] args)
         {
+            SendString sayHello, sayGoodbye, multiDelegate;
+            sayHello = Hello;
+            sayGoodbye = Goodbye;
+            multiDelegate = sayHello + sayGoodbye;
+            multiDelegate("문채영");
+            multiDelegate -= sayGoodbye;
+            multiDelegate("문채영");
+        }
+
+        private static void Goodbye(string message)
+        {
+            Console.WriteLine("안녕히가세요, " + message + "님.");
+        }
+
+        private static void Hello(string message)
+        {
+            Console.WriteLine("안녕하세요, " + message + "님.");
         }
     }
 }
